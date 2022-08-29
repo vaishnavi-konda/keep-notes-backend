e3d0ada750b1f677f95efb6a738e0af378dd2d6d
Vaishnavi Konda <kvaishnavi026@gmail.com>
2022-08-29 23:10:11 +0530

    Deployment

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGO_URL, () => {
   console.log('Connected to MongoDB Atlas successfully.');
 });
 
-app.use('https://rocky-ridge-07774.herokuapp.com/api/', homeRoute);
-app.use('https://rocky-ridge-07774.herokuapp.com/api/notes', notesRoute);
+app.use('/api/', homeRoute);
+app.use('/api/notes', notesRoute);
 
 app.listen(process.env.PORT||4000, (req, res) => {
   console.log('Listening on port 4000');
